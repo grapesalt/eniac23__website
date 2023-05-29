5ecbef4f69590d9b3e7c687efa8c308f1ee4230c
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2023-05-29 12:56:54 +0530

    added tailwind

--- app/root.jsx
+++ app/root.jsx
@@ -6,14 +6,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import stylesheet from "./styles/app.css";
 
 export const meta = () => [
   {
     charset: "utf-8",
-    title: "New Remix App",
+    title: "VIRUS 2023 | Eniac",
     viewport: "width=device-width,initial-scale=1",
   },
 ];
+
+export const links = () => [{ rel: "stylesheet", href: stylesheet }];
 
 export default function App() {
   return (
