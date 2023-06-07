ac0cf37393b0cefbe2b786e3b865849646c119a0
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2023-06-07 15:27:36 +0530

    fixed issue with lines on homepage and chamged subheading

--- app/components/sections/hero/homePage__mainText.jsx
+++ app/components/sections/hero/homePage__mainText.jsx
@@ -4,11 +4,11 @@ const Headings = () => {
       <h1 className="text-5xl md:text-7xl text-center mb-3 font-light z-50">
         VIRUS 2023
       </h1>
-      <p className="text-secondary text-md md:text-3xl hr-lines">
-        Breaking Boundaries to Metaverse
+      <p className="text-secondary text-xl md:text-3xl hr-lines tracking-wider md:tracking-wide  ">
+        New Realities: Metaverse
       </p>
-      <p className="text-secondary text-xs md:text-md text-right">
-        NEW REALITIES
+      <p className="text-secondary text-xs md:text-md text-right uppercase">
+        Breaking Boundaries
       </p>
     </>
   );
