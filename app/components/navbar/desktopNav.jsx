68385e43986f5721eadde527b645dcb0bbaf60f5
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2023-07-31 01:53:37 +0530

    added drop down to desktop menu

--- app/components/navbar/desktopNav.jsx
+++ app/components/navbar/desktopNav.jsx
@@ -1,29 +1,74 @@
 import { Link } from "@remix-run/react";
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import ActionButton from "../buttons/actionButton";
 import NavItem from "./navItem";
+import { FaAngleDown } from "react-icons/fa";
 
 const DesktopNav = () => {
+  const [isHomeHovered, setIsHomeHovered] = useState(false);
+  const pointerOnHome = () => {
+    setIsHomeHovered(true);
+  };
+  const pointerFromHome = () => {
+    setIsHomeHovered(false);
+  };
   return (
-    <nav
-      className={`items-center hidden md:flex fixed w-full mx-auto navbar px-6 h-[56px] z-[1000] top-0`}
-    >
-      <div className="flex w-full justify-between xl:max-w-[80lvw] mx-auto">
-        <Link to={"/"}>
-          <h1 className="text-2xl gugi">ENIAC</h1>
-        </Link>
-        <div className="flex justify-between items-center">
-          <NavItem text={"Home"} href={"/"} />
-          <NavItem text={"About Us"} href={"/"} canBeActive={false} />
-          <NavItem text={"Team"} href={"/"} canBeActive={false} />
-          <NavItem text={"Sponsors"} href={"/"} canBeActive={false} />
-          <NavItem text={"Events"} href={"/events"} />
-          <NavItem text={"Reach Out"} href={"/contact"} canBeActive={true} />
-          <div className="ml-6"></div>
-          <ActionButton className={"hidden lg:flex"} />
+    <>
+      <nav
+        className={`items-center hidden md:flex fixed w-full mx-auto navbar px-6 h-[56px] z-[1000] top-0`}
+      >
+        <div className="flex w-full justify-between xl:max-w-[80lvw] mx-auto">
+          <Link to={"/"}>
+            <h1 className="text-2xl gugi">ENIAC</h1>
+          </Link>
+          <div className="flex justify-between items-center">
+            <div className="flex flex-col">
+              <NavItem
+                text={"Home"}
+                icon={<FaAngleDown />}
+                href={"/"}
+                hovered={pointerOnHome}
+                notHovered={pointerFromHome}
+                noMl={true}
+              />
+              <div
+                className={`flex flex-col ${
+                  !isHomeHovered && `hidden`
+                } absolute pt-7 z-10`}
+                onMouseEnter={pointerOnHome}
+                onMouseLeave={pointerFromHome}
+              >
+                <div className=" bg-black/70 p-0 m-0">
+                  <NavItem
+                    noMl={true}
+                    canBeActive={false}
+                    text={"Sponsors"}
+                    href={"/"}
+                  />
+                  <NavItem
+                    noMl={true}
+                    canBeActive={false}
+                    text={"About"}
+                    href={"/"}
+                  />
+                  <NavItem
+                    noMl={true}
+                    canBeActive={false}
+                    text={"Team"}
+                    href={"/"}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <NavItem text={"Events"} href={"/events"} />
+            <NavItem text={"Reach Out"} href={"/contact"} canBeActive={true} />
+            <div className="ml-6"></div>
+            <ActionButton className={"hidden lg:flex"} />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
--- app/components/navbar/navItem.jsx
+++ app/components/navbar/navItem.jsx
@@ -1,19 +1,38 @@
 import { NavLink } from "@remix-run/react";
 import React from "react";
 
-const NavItem = ({ text, href, canBeActive }) => {
+const NavItem = ({
+  text,
+  href,
+  canBeActive,
+  icon,
+  hovered,
+  notHovered,
+  noMl,
+}) => {
   canBeActive = canBeActive ?? true;
   return (
     <NavLink
       to={href}
       prefetch="intent"
+      onMouseEnter={hovered && hovered}
+      onMouseLeave={notHovered && notHovered}
       className={({ isActive }) => {
         return canBeActive && isActive
-          ? `ml-6 text-white nav-item-shadow text-sm transition-all duration-300`
-          : `ml-6 text-[#d9d9d9] text-sm hover:text-white transition-all duration-300`;
+          ? `${
+              !noMl && `ml-6`
+            } text-white nav-item-shadow text-sm transition-all duration-300 flex items-center z-50`
+          : `${
+              !noMl && `ml-6`
+            } text-[#d9d9d9] text-sm hover:text-white transition-all duration-300 flex items-center z-50`;
       }}
     >
       {text}
+      {icon && (
+        <>
+          <div className="ml-[2px]">{icon}</div>
+        </>
+      )}
     </NavLink>
   );
 };
