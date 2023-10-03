94c99d5c4b52aa1f8fe0c4b5ae2b96ac1a1f686b
Aditya Chaturvedi <aditya.chaturvedi1608@gmail.com>
2023-10-04 01:45:52 +0530

    Rounded corners

--- app/components/sections/events/events.jsx
+++ app/components/sections/events/events.jsx
@@ -28,7 +28,7 @@ const Events = () => {
           <Link to={`/events/${event.permalink}`}>
             <div className="relative overflow-hidden bg-cover bg-no-repeat">
               <img className="md:rounded-md object-cover h-full w-full" src={images[event.permalink]} alt="" />
-              <div className="absolute rounded-md bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 opacity-0 transition duration-300 ease-in-out hover:opacity-70"></div>
+              <div className="absolute md:rounded-md bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 opacity-0 transition duration-300 ease-in-out hover:opacity-70"></div>
             </div>
           </Link>
         );
