ac013aa1330fa2e915801433e6984b5ca30afd39
Aditya Chaturvedi <aditya.chaturvedi1608@gmail.com>
2023-10-09 07:11:20 +0530

    Images in event page

--- app/components/buttons/actionButton.jsx
+++ app/components/buttons/actionButton.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { BsArrowUpRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
-const ActionButton = ({ text, className, link }) => {
+const ActionButton = ({ text, className, link = "", onClick }) => {
 
   return (
     <div
+      onClick={onClick}
       className={`flex text-center justify-center uppercase action-button-bg py-[6.9px] px-5 text-sm hover:cursor-pointer transition-all duration-300 ${className}`}
     >
       <Link className="flex" to={link}>
--- app/routes/events.$event.jsx
+++ app/routes/events.$event.jsx
@@ -5,6 +5,15 @@ import { Editor, EditorProvider } from "react-simple-wysiwyg";
 import styles from "../styles/eventInfo.css";
 const base64 = require("js-base64");
 
+import armageddon from '../data/images/armageddon_logo.png'
+import brainstorm from '../data/images/brainstorm_logo.png'
+import cinephoria from '../data/images/cinephoria_logo.png'
+import click_clash from '../data/images/click-clash_logo.png'
+import codequest from '../data/images/codequest_logo.png'
+import educraft from '../data/images/educraft_logo.png'
+import meme_o_mania from '../data/images/meme-o-mania_logo.png'
+import surprise from '../data/images/surprise_logo.png'
+
 export const links = () => [
   {
     rel: "stylesheet",
@@ -16,6 +25,17 @@ const MemePage = () => {
   const params = useParams();
   const eventData = events.find((e) => e.permalink === params.event);
 
+  const images = {
+    'surprise': surprise,
+    'meme-o-mania': meme_o_mania,
+    'educraft': educraft,
+    'codequest': codequest,
+    'click-clash': click_clash,
+    'cinephoria': cinephoria,
+    'brainstorm': brainstorm,
+    'armageddon' : armageddon,
+  }
+
   const es = base64.decode(eventData.data);
 
   return (
@@ -23,6 +43,8 @@ const MemePage = () => {
       <div className="text-center md:text-left text-2xl md:text-3xl pt-2">
         {eventData.title}
       </div>
+      <img className="m-10 w-[15rem] h-[15rem]" src={images[eventData.permalink]} alt={eventData.title} /> 
+
       <div
         className="events-info"
         dangerouslySetInnerHTML={{ __html: es }}
